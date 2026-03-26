ea998c8517b3387e0a2a34e36f8e4aca196064e9
Edith@1887 <augustuscalvinsingh@gmail.com>
2026-03-26 09:23:47 +0530

    C:\Users\Admin\Desktop\RA2411047010091

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,19 @@
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args){
+      /* System.out.println("=========================================");
+        System.out.println("welcome to Book My Stay App");
+        System.out.println("Hotel booking system v3.0");
+        System.out.println("System Started Successfully");
+        System.out.println("=========================================");*/
+
+        int attempt = 1;
+        do{
+                System.out.println("attempt" + attempt);
+                attempt++;
+        } while (attempt <= 0);
+
+
+
+    }
+
+}
